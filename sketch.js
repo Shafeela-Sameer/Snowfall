2a0fd6d0dbbd2f8b1d723c16d93440d0b09c9754
Kheya-Mandal <76730767+Kheya-Mandal@users.noreply.github.com>
2021-07-22 16:07:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var Engine = Matter.Engine,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+var backgroundImg;
+var snow = [];
+
+    function preload( ) {
+          backgroundImg = loadImage("snow3.jpg");
+      }
+    
+function setup() {
+  createCanvas(1200,1000);
+  //createSprite(400, 200, 50, 50);
+  engine = Engine.create();
+  world = engine.world;
+//creating snow
+  for(var a = 50; a <=width; a=a+20){
+
+  
+  }
+}
+
+function draw() {
+  background(backgroundImg); 
+
+  if(frameCount % 20 === 0){ 
+    snow.push(new Snow(random (10,1000),10));
+  }
+  
+  Engine.update(engine);
+
+  for (var a = 0; a < snow.length; a++) {
+    
+ snow[a].display();
+}
+  
+  drawSprites();
+}
